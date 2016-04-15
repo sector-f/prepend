c44b0410b21c1110e4f96c30370e93ef93d0e886
Adam <sector-f on github>
2016-04-15 11:19:36 -0400

    Added support for multiple simultaneous files

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # prepend
 
-Command-line utility for adding text to the start of a file.  
-Input text is received through standard input. Output file is specified as an argument.
+Command-line utility for adding text to the start of files.
+Input text is received through standard input. At least one output file is specified as an argument.
 
 **Warning:** This program functions by truncating the file to zero length before writing data to it.
-Data loss is possible if the program is killed or interrupted in any way. Use at your own risk.
+Data loss is possible if the program is killed or interrupted in any way.
+However, this is most likely a small risk, probably equivalent to the chance of data loss using the redirect feature of a shell (`>`).
 
 ### Example usage:
 
--- src/prepend.rs
+++ src/prepend.rs
@@ -1,17 +1,9 @@
 use std::fs::OpenOptions;
 use std::io::{self, Read, Write};
 use std::process::exit;
-use std::env;
+use std::{env, ffi};
 
-fn main() {
-    let file = match env::args_os().nth(1) {
-        Some(f) => f,
-        None => {
-            println!("No file specified");
-            exit(1);
-        },
-    };
-
+fn prepend(stdin_buffer: &String, file: ffi::OsString) {
     let mut openfile = match OpenOptions::new()
         .read(true)
         .write(true)
@@ -24,13 +16,24 @@ fn main() {
             },
         };
 
-    let mut buffer = String::new();
-    let _ = openfile.read_to_string(&mut buffer);
-
-    let mut stdin = String::new();
-    let _ = io::stdin().read_to_string(&mut stdin);
+    let mut file_buffer = String::new();
+    let _ = openfile.read_to_string(&mut file_buffer);
 
     let _ = openfile.set_len(0);
-    let _ = openfile.write(stdin.as_bytes());
-    let _ = openfile.write(buffer.as_bytes());
+    let _ = openfile.write(stdin_buffer.as_bytes());
+    let _ = openfile.write(file_buffer.as_bytes());
+}
+
+fn main() {
+    if env::args_os().count() < 2 {
+        println!("No file(s) specified");
+        exit(1);
+    }
+
+    let mut stdin_buffer = String::new();
+    let _ = io::stdin().read_to_string(&mut stdin_buffer);
+
+    for file in env::args_os().skip(1) {
+        prepend(&stdin_buffer, file);
+    }
 }
