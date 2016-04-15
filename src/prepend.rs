b12bbd2868524b847ffd2c593315753fc60a810a
Adam <sector-f on github>
2016-04-15 14:28:48 -0400

    Use stderr for error messages

--- src/prepend.rs
+++ src/prepend.rs
@@ -22,21 +22,25 @@ fn prepend(stdin_buffer: &Vec<u8>, file: &ffi::OsString) -> io::Result<()> {
     Ok(())
 }
 
+fn print_error(error: String) {
+    let _ = io::stderr().write_all(error.as_bytes());
+}
+
 fn main() {
     if env::args_os().count() < 2 {
-        println!("No file(s) specified");
+        print_error(format!("No file(s) specified\n"));
         exit(1);
     }
 
     let mut stdin_buffer: Vec<u8> = Vec::new();
     if let Err(e) = io::stdin().read_to_end(&mut stdin_buffer) {
-        println!("Failed to read from stdin: {}", e);
+        print_error(format!("Failed to read from stdin: {}\n", e));
         exit(1);
     }
 
     for file in env::args_os().skip(1) {
         if let Err(e) = prepend(&stdin_buffer, &file) {
-            println!("Writing to file {} failed: {}", file.to_string_lossy(), e);
+            print_error(format!("Writing to file {} failed: {}\n", file.to_string_lossy(), e));
         }
     }
 }
