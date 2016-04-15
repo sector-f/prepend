dd1e97e8b3976bb5f92978fdb3f0b6464e9ed74d
Adam <sector-f on github>
2016-04-15 17:21:55 -0400

    Minor change in error handling

--- src/prepend.rs
+++ src/prepend.rs
@@ -22,6 +22,18 @@ fn prepend(stdin_buffer: &Vec<u8>, file: &ffi::OsString) -> io::Result<()> {
     Ok(())
 }
 
+fn can_write(file: &ffi::OsString) -> bool {
+    if let Err(e) = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(file) {
+                    print_error(format!("Writing to file {} failed: {}\n", file.to_string_lossy(), e));
+                    false
+            } else {
+                true
+            }
+}
+
 fn print_error(error: String) {
     let _ = io::stderr().write_all(error.as_bytes());
 }
@@ -32,13 +44,25 @@ fn main() {
         exit(1);
     }
 
+    let mut files_vec: Vec<ffi::OsString> = Vec::new();
+
+    for file in env::args_os().skip(1) {
+        if can_write(&file) {
+            files_vec.push(file);
+        }
+    }
+
+    if files_vec.len() == 0 {
+        exit(1);
+    }
+
     let mut stdin_buffer: Vec<u8> = Vec::new();
     if let Err(e) = io::stdin().read_to_end(&mut stdin_buffer) {
         print_error(format!("Failed to read from stdin: {}\n", e));
         exit(1);
     }
 
-    for file in env::args_os().skip(1) {
+    for file in files_vec {
         if let Err(e) = prepend(&stdin_buffer, &file) {
             print_error(format!("Writing to file {} failed: {}\n", file.to_string_lossy(), e));
         }
